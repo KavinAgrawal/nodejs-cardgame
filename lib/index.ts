b341716d423afb6ad584d2fbb38e4c7b883347b8
Kavin Agrawal <kavin.agrawal@gmail.com>
2023-02-24 19:04:49 +0530

    Added inline game

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,24 @@
 import { GameCards } from "./GameCards";
+import * as readline from "readline";
 
 const gameCards = new GameCards();
-gameCards.playGame();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const playGame = () => {
+  rl.question("Enter 1 to play game, 2 to exit: ", (answer) => {
+    if (answer === "1") {
+      gameCards.playGame();
+      playGame();
+    } else if (answer === "2") {
+      rl.close();
+    } else {
+      console.log("Invalid input");
+      playGame();
+    }
+  });
+};
+
+playGame();
